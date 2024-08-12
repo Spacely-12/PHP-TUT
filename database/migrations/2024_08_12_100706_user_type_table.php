e36fbd120e1a90f3a0716dc7c9f9d92e57cf2eae
Salawudeen <mohammedsalawudeen99@gmail.com>
2024-08-12 11:59:16 +0000

    chore: Add Tailwind CSS and Alpine.js to project dependencies

--- database/migrations/2024_08_12_100706_user_type_table.php
+++ database/migrations/2024_08_12_100706_user_type_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB; // Add this line
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -11,14 +12,21 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('user_types', function (Blueprint $table) {
+            $table->id();
+            $table->string('type')->unique();
+            $table->timestamps();
+        });
+
+        // Insert default user types
+        DB::table('user_types')->insert([
+            ['type' => 'admin'],
+            ['type' => 'user'],
+        ]);
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('user_types');
     }
 };
